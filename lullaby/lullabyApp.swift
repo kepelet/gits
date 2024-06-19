5456724c1fa21a032de6ffb47563b00f1072641a
Alvin Miftah <azraqazravin@gmail.com>
2024-06-19 17:35:04 +0700

    Initial Commit

---
+++ lullaby/lullabyApp.swift
@@ -0,0 +1,17 @@
+//
+//  lullabyApp.swift
+//  lullaby
+//
+//  Created by Alvin Miftah on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct lullabyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
